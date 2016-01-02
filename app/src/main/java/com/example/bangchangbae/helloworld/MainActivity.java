486c0bd2205f51070e7163c1c7ba11012ee92a1d
bangchangbae <bangchangbae@gmail.com>
2016-01-02 11:27:10 +0900

    Activity Event Handling 추가

--- app/src/main/java/com/example/bangchangbae/helloworld/MainActivity.java
+++ app/src/main/java/com/example/bangchangbae/helloworld/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(MainActivity.this, "clicked settings", Toast.LENGTH_SHORT).show();
             return true;
         }
 
